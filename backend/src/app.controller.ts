a55ffa4ebad246b9be765d958f445a3e8fdbcba5
Khalil <h.sultanoff@yandex.ru>
2025-05-11 17:17:58 +0300

    lab 1

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,57 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Query, Render} from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    @Get()
+    @Render('index')
+    getHome(@Query('auth') auth: string) {
+        const user = auth === '1' ? 'Иван' : null;
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+        const brands = Array(12).fill('ВАЗ');
+
+        const popular = Array(6).fill(null).map(() => ({
+            name: 'Кардан',
+            manufacturer: 'Россия',
+            price: '1500р',
+            img: '/img/cardan.jpg',
+        }));
+
+        return {user, brands, popular};
+    }
+
+
+    @Get('/pay')
+    @Render('pay')
+    getPay() {
+        return {user: null};
+    }
+
+    @Get('/delivery')
+    @Render('delivery')
+    getDeliveryPage() {
+        return {
+            title: 'Доставка',
+            user: null, // или текущий пользователь, если есть
+        };
+    }
+
+    @Get('/reviews')
+    @Render('reviews')
+    getReviewsPage() {
+        return {
+            title: 'Отзывы',
+            user: null, // или 'Иван', если нужен авторизованный пользователь
+        };
+    }
+
+    @Get('/contacts')
+    @Render('contacts')
+    getContactsPage() {
+        return {
+            title: 'Контакты',
+            user: null, // или имя, если залогинен
+        };
+    }
+
+
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {NestExpressApplication} from '@nestjs/platform-express';
+import {join} from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    // Папка для представлений (views)
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+
+    // Шаблонизатор Handlebars
+    app.setViewEngine('hbs');
+
+    // Подключение partials (общих блоков)
+    hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+
+    app.set('view options', {
+        layout: 'layout', // <== вот это обязательно!
+    });
+
+
+    // Отдача статики из папки public (css, js, html)
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+
+    // Порт из переменной окружения или 3000 по умолчанию
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
 }
+
 bootstrap();
